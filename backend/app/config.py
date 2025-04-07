34a3163361dca78d11f30f27b2cc8bf5809562f9
sujay malghan <sujayvmalghan@gmail.com>
2025-04-06 21:32:06 -0600

    config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,12 @@
 import os
 from urllib.parse import quote_plus
 
-RDS_HOST = os.environ.get('RDS_HOST', 'sujayvm.mysql.polardb.eu-west-1.rds.aliyuncs.com')
-RDS_PORT = os.environ.get('RDS_PORT', '3306')
-RDS_DB = os.environ.get('RDS_DB', 'robot')
-RDS_USER = os.environ.get('RDS_USER', 'root_name')
+RDS_HOST = os.environ.get('RDS_HOST', '')
+RDS_PORT = os.environ.get('RDS_PORT', '')
+RDS_DB = os.environ.get('RDS_DB', '')
+RDS_USER = os.environ.get('RDS_USER', '')
 # Replace YOUR_DB_PASSWORD with your actual password that contains '@'
-raw_password = os.environ.get('RDS_PASSWORD', 'sujay@HOME7')
+raw_password = os.environ.get('RDS_PASSWORD', '7')
 RDS_PASSWORD = quote_plus(raw_password)  # URL-encode the password
 
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DB}"
